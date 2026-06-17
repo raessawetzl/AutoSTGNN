e66bb17e41a5964001e6a4705e68cb561cf7fa60
Khaleel Hassan <khaleelhassan246@gmail.com>
2026-06-17 14:23:08 +0200

    Added PEMS8

--- test.py
+++ test.py
@@ -1,7 +1,19 @@
 import numpy as np
+import pandas as pd
 
-data = np.load("data/PEMS04.npz")
+p4_data = np.load("data/PEMS04.npz")
+p4_flow = p4_data['data']
+p4_adj = pd.read_csv('data/PEMS04.csv', header=None)
 
-flow = data['data']
+p8_data = np.load("data/PEMS08.npz")
+p8_flow = p8_data['data']
+p8_adj = pd.read_csv('data/PEMS08.csv', header = None)
 
-print(flow.shape)
+print(p4_flow.shape)
+print(p8_flow.shape)
+
+print(p4_adj.head())
+
+print(p8_adj.head())
+
+
